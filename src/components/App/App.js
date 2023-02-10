d6ac879d160323a6a29a032358c0f98ffe5d9d38
Linssi Colin Ruiz <linssi101@gmail.com>
2023-02-09 19:04:51 -0600

    created details router - added code comments

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,30 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import MovieList from '../MovieList/MovieList'
+
+// import from MovieList
+import MovieList from '../MovieList/MovieList';
+
+// import from Details
+import Details from '../Details/Details';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>The Movies Saga!</h1>
-      <Router>        
-        <Route path="/" exact>
-          <MovieList />
-        </Route>
-        
-        {/* Details page */}
+	return (
+		<div className="App">
+			<h1>The Movies Saga!</h1>
+			<Router>
+				<Route path="/" exact>
+					<MovieList />
+				</Route>
 
-        {/* Add Movie page */}
-      </Router>
-    </div>
-  );
+				{/* Details page */}
+				<Router>
+					<Details />
+				</Router>
+
+				{/* Add Movie page */}
+			</Router>
+		</div>
+	);
 }
-
 
 export default App;
