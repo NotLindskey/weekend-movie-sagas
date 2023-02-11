db63bf7ca3fab9b6607233abf66052b7e7e92139
Linssi Colin Ruiz <linssi101@gmail.com>
2023-02-11 16:22:29 -0600

    created "back" button that routes to movie list on click

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import { useHistory } from "react-router-dom";
+
 function details() {
+
+    const history = useHistory();
+
+    const sendToHome = () => {
+        console.log('to home page!');
+        history.push('/')
+    }
+
     return (
         <>
         <div>details</div>
         <p>poster clicked - [x]</p>
+        <br />
+        <button onClick={sendToHome}>back</button>
         </>
     )
 }
