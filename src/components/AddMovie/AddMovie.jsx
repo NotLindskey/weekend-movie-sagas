d173610e33619f643a60f5463e64eb5f520cd500
Linssi Colin Ruiz <linssi101@gmail.com>
2023-02-18 16:30:57 -0600

    created inputs to capture inputs.

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,12 +1,90 @@
 import React from 'react';
 
+import { useDispatch, useSelector } from 'react-redux'
+import { useState } from 'react'
+
 function AddMovie() {
-    console.log('inside AddMovie Component, working!')
+
+    const dispatch = useDispatch();
+
+    // A // add title
+    const [title, setTitle] = useState('');
+    // event
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    // B // add poster
+    const [poster, setPoster] = useState('');
+    // event
+    const handlePosterChange = (event) => {
+        setPoster(event.target.value);
+    }
+
+    // C // add genre
+    const [genre, setGenre] = useState('');
+     // event
+    const handleGenreChange = (event) => {
+        setGenre(event.target.value);
+    }
+
+    // D // add description
+    const [description, setDescription] = useState('');
+    // event
+    const handleDescriptonChange = (event) => {
+        setDescription(event.target.value);
+    }
+
+    // on submit > event
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('title:', title);
+        console.log('poster:', poster);
+        console.log('genre:', genre);
+        console.log('description:', description);
+        dispatch({
+            type: 'ADD_MOVIE',
+            payload: 
+                setTitle,
+                setPoster,
+                setGenre,
+                setDescription
+        });
+        setTitle('');
+        setPoster('');
+        setGenre('');
+        setDescription('');
+    }    
 
     return (
-        <section>
-        <h1>hi</h1>
-        </section>
+        <div>
+            <form onSubmit={handleSubmit}>
+            <p>Add Movie:</p>
+        
+                {/* /title */}
+                <input 
+                    value={title} onChange={handleTitleChange} type="text" id="title" name="title" placeholder='title Here'>
+                </input>
+
+                {/* /image */}
+                <input 
+                    value={poster} onChange={handlePosterChange} type="text" id="poster" name="poster" placeholder='poster Here'>
+                </input>
+
+                {/* /genre */}
+                <input 
+                    value={genre} onChange={handleGenreChange} type="text" id="genre" name="genre" placeholder='genre Here'>
+                </input>
+
+                {/* /descripton */}
+                <input 
+                    value={description} onChange={handleDescriptonChange} type="text" id="descripton" name="descripton" placeholder='descripton Here'>
+                </input>
+
+                {/* submit */}
+                <button type="submit">Enter your movie!</button>
+            </form>
+        </div>
     )
 }
 
