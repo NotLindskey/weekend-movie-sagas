c95307c3ffb96a892949dbb7afba95fa749be81c
Linssi Colin Ruiz <linssi101@gmail.com>
2023-02-20 21:30:57 -0600

    removed code comments, added handle to movie route

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,41 +1,33 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { useHistory } from 'react-router-dom';
-import AddMovie from '../AddMovie/AddMovie';
-
 import './MovieList.css'
 
 function MovieList() {
-
-    const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
+    const dispatch = useDispatch();
+    const history = useHistory();
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
 
-    const history = useHistory();
+    // add movie clicked - send to addMovie
+    const handleToMovie = () => {
+        console.log('proceed to add movie?')
+        history.push('/addMovie')
+    }
 
-
-    // click to details 
+    // poster clicked - send to movie details 
     const handleClick = (movie) => {
         alert(`you clicked ${movie.title}!`);
         history.push('/details');
         
     }
 
-    // // click to addMovie
-    // const addButton = () => {
-    //     if (confirm('proceed to add your favorite movie?') == true) {
-    //         history.push('/addMovie');
-    //     }
-    // }
-
     return (
         <main>
-            <AddMovie />
-
+            <button onClick={() => handleToMovie()}>Add movie</button>
             <h1>MovieList</h1>
             <section className="movies">
                 {movies.map(movie => {
